1beb15d166852e7755ad05ad281e14c1ef695029
Maidelin R <maidelin.rubio@appliedtechnology.se>
2024-01-05 18:59:54 +0100

    add scope in program

--- RandomThoughtsApi/Program.cs
+++ RandomThoughtsApi/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
+using RandomThoughtsApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IThoughtsRepository, ThoughtsRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
